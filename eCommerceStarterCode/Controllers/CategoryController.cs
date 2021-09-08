de0ae33935eaaf4e6cbbe4189bd5ffef8be3f628
Steve Clark <steve.a.clark48@gmail.com>
2021-09-07 21:03:28 -0500

    added controls for categories

--- eCommerceStarterCode/Controllers/CategoryController.cs
+++ eCommerceStarterCode/Controllers/CategoryController.cs
@@ -41,6 +41,26 @@ namespace eCommerceStarterCode.Controllers
             _context.SaveChanges();
             return StatusCode(201, value);
         }
+        [HttpGet("catagoryResults")]
+        public IActionResult GetAllCategories(int categoryId)
+        {
+            // get all categories
+            var categories = _context.Category.Where(p => p.CategoryId == categoryId);
+            var categoriesWithProduct = from c in categories
+                                        join cId in _context.Products
+                                        on c.CategoryId equals cId.CategoryId
+                                        select new
+                                        {
+                                            ProductId = cId.ProductId,
+                                            ProductName = cId.ProductName,
+                                            ProductPrice = cId.ProductPrice,
+                                            ProductDescription = cId.ProductDescription,
+                                            CategoryId = c.CategoryId,
+                                            CategoryName = c.CategoryName
+                                        };
+
+            return Ok(categoriesWithProduct);
+        }
 
     }
 }
